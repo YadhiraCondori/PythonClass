fa7628434b8809791b9c2e6a34839a3a6aff45e7
Yadhirline09 <69084308+Yadhirline09@users.noreply.github.com>
2020-08-06 19:05:09 -0500

    Ejerc

---
+++ CLASE 2/Convertidor_moneda.py
@@ -0,0 +1,6 @@
+print("ejercicio convetidor")
+tipo_de_cambio = 0.28
+print ("ingresa el monto en soles")
+monto_soles = float(print)
+monto_dolares = round(monto_soles * tipo_de_cambio,2)
+print("El monto en dolares es: "  + str(monto_dolares))
