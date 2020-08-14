d347b06fce7ddaa971bfc421dd434d82ee042b22
Yadhirline09 <69084308+Yadhirline09@users.noreply.github.com>
2020-08-13 19:02:26 -0500

    while and for

---
+++ PRACTICA 1/CLASE 3.py
@@ -0,0 +1,5 @@
+palabra = input("Ingrese una palabra: ")
+for i in palabra:
+    if i == "o":
+        break
+    print(i)
---
+++ pratica2_while_for/while.py
@@ -0,0 +1,6 @@
+contador = 0
+while contador <= 100:
+    if contador == 20:
+       break
+    contador = contador +1
+    print(contador)
